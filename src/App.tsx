c0d1c492850f28e6bdc0a210b594e9a83da81e04
pbr1111 <pbr1111@gmail.com>
2019-10-28 20:49:06 +0100

    Change firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,11 @@ import '@ionic/react/css/structure.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/typography.css';
-import * as firebase from 'firebase/app'
 import React from 'react';
-import { firebaseConfig } from './config/firebase';
 import { Routes } from './routes/Routes';
 import './theme/variables.css';
 import './config/i18n';
-
-firebase.initializeApp(firebaseConfig);
+import './config/firebase';
 
 const App: React.FC = () => (
     <IonApp>
